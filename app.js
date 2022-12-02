7ce32c78945b664f9012c358b484e1feca2d02e3
toocels <toocels@gmail.com>
2022-12-02 22:34:33 +0000

    Basic server class

--- app.js
+++ app.js
@@ -2,43 +2,63 @@ require('dotenv').config();
 const os = require('os');
 const fs = require('fs');
 const express = require('express');
-const app = express();
+
 
 const SERVER_IP = "localhost";
 const SERVER_PORT = 80; // process.env.SERVER_PORT
 
 
-console.log("Starting server...")
+class server {
+	constructor(SERVER_IP, SERVER_PORT){
+		this.SERVER_PORT = SERVER_PORT
+		this.SERVER_IP = SERVER_IP
+		this.app = express();
+		this.app.use(express.static("public"))
+	}
 
-app.use(express.static("public"))
-
-app.get('*', (req,res) => {
-	// res.sendFile(__dirname + "/public/index.html")
-	// res.render("public/index")
-	// res.download("app.js")
-	// res.json({"message":"error"});
-	// res.sendStatus(500);
-	res.send("Hi");
-	// res.redirect("/page")
-	console.log("[ APP ]  Req path: " + req.path)
-})
-
-
-app.post('*', (req, res) => {
-    let body = '';
-    req.on('data', data => body += data)
-    req.on('end', () => {
-        res.writeHead(200, {
-            'Content-Type': 'application/json',
-            'Access-Control-Allow-Origin': '*',
-            'Access-Control-Allow-Methods': 'POST, GET',
+	start_server(){
+		this.app.listen(this.SERVER_PORT, this.SERVER_IP, error => {
+            if (error) {
+                console.log("Error in server startup.");
+            } else {
+                console.log('Example app listening at http://' + this.SERVER_IP + ":" + this.SERVER_PORT)
+            }
         });
-        body=JSON.parse(body)
-    });
+	}
 
-    //write code from here
-    res.end("Ok");
-});
+	get_req(){
+		this.app.get('*', (req,res) => {
+			// res.sendFile(__dirname + "/public/index.html")
+			// res.render("public/index")
+			// res.download("app.js")
+			// res.json({"message":"error"});
+			// res.sendStatus(500);
+			res.send("Hi");
+			// res.redirect("/page")
+			console.log("[ APP ]  Req path: " + req.path)
+		})
+	}
+
+	post_req(){
+		this.app.post('*', (req, res) => {
+		    let body = '';
+		    req.on('data', data => body += data)
+		    req.on('end', () => {
+		        res.writeHead(200, {
+		            'Content-Type': 'application/json',
+		            'Access-Control-Allow-Origin': '*',
+		            'Access-Control-Allow-Methods': 'POST, GET',
+		        });
+		        body=JSON.parse(body)
+		    });
+
+		    //write code from here
+		    res.end("Ok");
+		});		
+	}
+}
 
 
-app.listen(SERVER_PORT)
+console.log("Starting server...")
+let main_server = new server(SERVER_IP, SERVER_PORT)
+main_server.start_server()
